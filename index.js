1f77fb32d200832250ce311861de89ade7ae955b
Jordy Tshibangu <jordytshibss@gmail.com>
2021-05-05 07:22:38 +0200

    test-4 : number of occurence of a voyel in a expression

--- index.js
+++ index.js
@@ -3,4 +3,6 @@ const { palindorme } = require('./easy/test-1');
 const { reverseString } = require('./easy/test-2')
 // reverseString("hello")
 const { checkOccurence } = require('./easy/test-3')
-checkOccurence("hello", "l")
+// checkOccurence("hello", "l")
+const { countVoyelNumber } =require('./easy/test-4')
+countVoyelNumber("JavaScript program")
